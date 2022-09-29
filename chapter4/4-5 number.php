e53b3383f0e88a876b1cc16a1a9e6f9f4535730e
jaegeon cha <jaegun920@naver.com>
2022-09-29 17:20:24 +0900

    echo

---
+++ chapter4/4-5 number.php
@@ -0,0 +1,33 @@
+<?php
+    echo 1;
+    print 1;
+?>
+<!DOCTYPE html>
+    <head>
+        <title>
+            산술 연산자
+        </title>
+    </head>
+    <body>
+        <h1>Number & Arithmetic Operator</h1>
+        <h2>1 + 1(덧셈 연산)</h2>
+        <?php
+            echo 1 + 1;
+        ?>
+        <h2>2 - 1(뺄셈 연산)</h2>
+        <?php
+            echo 2 - 1;
+        ?>
+        <h2>2 * 2(곱셈 연산)</h2>
+
+        <?php
+            echo 2 * 2;
+        ?>
+
+        <h2>4 / 2(나눗셈 연산)</h2>
+        <?php
+            echo 4 / 2;
+        ?>
+
+    </body>
+</html>
