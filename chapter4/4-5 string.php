742b27bc5272096b33d4a30f81388c21b7f7a0ca
jaegeon cha <jaegun920@naver.com>
2022-09-29 13:54:46 +0900

    init commit

---
+++ chapter4/4-5 string.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            문자열 출력하기
+        </title>
+    </head>
+    <body>
+        <h1>String & String Operator</h1>
+        <?php
+            echo "Hello 'W'orld";
+            echo "Hello \"W\"orld";
+        ?>
+
+        <?php
+           echo "Hell"."World";
+        ?>
+        <h2>String length function</h2>
+        <?php
+            $a = "Hello 'W'orld";
+            echo strlen($a);
+        ?>
+
+    </body>
+</html>
+
---
+++ chapter4/4-6 variable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        변수를 이용한 덧셈, 변수활용
+    </title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php
+    $a = 10;
+    echo $a + 1;
+    ?>
+    <?php $name = " leezche";
+    echo"Lorem ipsum dolor sit amet consectetur".$name." adipisicing elit.
+    Voluptatum non eaque impedit fugiat delectus ipsa ".$name."laudantium hic, at laborum, explicabo,
+    beatae numquam et recusandae ".$name."voluptatem animi eius possimus deleniti laboriosam!"
+    ?>
+</body>
+</html>
